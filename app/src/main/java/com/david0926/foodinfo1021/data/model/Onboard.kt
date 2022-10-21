733698ada644e4142502d5d8930c2960b6f19357
roian6 <roian6@naver.com>
2022-10-21 22:08:39 +0900

    Initial commit

---
+++ app/src/main/java/com/david0926/foodinfo1021/data/model/Onboard.kt
@@ -0,0 +1,7 @@
+package com.david0926.foodinfo1021.data.model
+
+data class Onboard(
+    val lottieUrl: String,
+    val title: String,
+    val description: String
+)
---
+++ app/src/main/java/com/david0926/foodinfo1021/ui/onboard/OnboardActivity.kt
@@ -0,0 +1,12 @@
+package com.david0926.foodinfo1021.ui.onboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.david0926.foodinfo1021.R
+
+class OnboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboard)
+    }
+}
